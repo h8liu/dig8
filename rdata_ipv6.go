49ca490e90f72bc00f8e6c360a4e6532b7879ff0
He Liu <liulonnie@gmail.com>
2014-12-31 23:13:39 -0800

    rdata

---
+++ rdata_ipv6.go
@@ -0,0 +1,34 @@
+package dig8
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+)
+
+// RdIPv6 is a IPv6 rdata
+type RdIPv6 net.IP
+
+// UnpackRdIPv6 unpacks an IPv6 record
+func UnpackRdIPv6(in *bytes.Reader, n uint16) (RdIPv6, error) {
+	if n != 16 {
+		return nil, fmt.Errorf("IPv6 with %d bytes", n)
+	}
+	buf := make([]byte, 16)
+	_, e := in.Read(buf)
+	if e != nil {
+		return nil, e
+	}
+
+	return RdIPv6(buf), nil
+}
+
+// PrintTo prints the record to output
+func (d RdIPv6) PrintTo(out *bytes.Buffer) {
+	fmt.Fprint(out, net.IP(d))
+}
+
+// Pack packs the record to bytes
+func (d RdIPv6) Pack() []byte {
+	return net.IP(d).To16()
+}
---
+++ rdata_soa.go
@@ -0,0 +1,81 @@
+package dig8
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// RdSoa is an SOA rdata
+type RdSoa struct {
+	Mname                  []string
+	Rname                  []string
+	Serial, Refresh        uint32
+	Retry, Expire, Minimum uint32
+}
+
+// PrintTo prints the thing to output
+func (d *RdSoa) PrintTo(out *bytes.Buffer) {
+	fmt.Fprintf(out, "%v/%v serial=%d refresh=%d retry=%d exp=%d min=%d",
+		strings.Join(d.Mname, "."),
+		strings.Join(d.Rname, "."),
+		d.Serial, d.Refresh, d.Retry, d.Expire, d.Minimum)
+}
+
+// UnpackRdSoa unpacks the thing
+func UnpackRdSoa(in *bytes.Reader, n uint16, p []byte) (*RdSoa, error) {
+	if n <= 22 {
+		return nil, fmt.Errorf("soa with %d bytes", n)
+	}
+
+	ret := new(RdSoa)
+	was := in.Len()
+	labels, e := UnpackLabels(in, p)
+	if e != nil {
+		return nil, e
+	}
+	ret.Mname = labels
+
+	labels, e = UnpackLabels(in, p)
+	if e != nil {
+		return nil, e
+	}
+	ret.Rname = labels
+
+	now := in.Len()
+	if was-now+20 != int(n) {
+		return nil, errors.New("invalid soa field length")
+	}
+
+	buf := make([]byte, 20)
+	_, e = in.Read(buf)
+	if e != nil {
+		return nil, e
+	}
+	ret.Serial = enc.Uint32(buf[0:4])
+	ret.Refresh = enc.Uint32(buf[4:8])
+	ret.Retry = enc.Uint32(buf[8:12])
+	ret.Expire = enc.Uint32(buf[12:16])
+	ret.Minimum = enc.Uint32(buf[16:20])
+
+	return ret, nil
+}
+
+// Pack packs the thing.
+func (d *RdSoa) Pack() []byte {
+	buf := new(bytes.Buffer)
+	PackLabels(buf, d.Mname)
+	PackLabels(buf, d.Rname)
+
+	b := make([]byte, 20)
+	enc.PutUint32(b[0:4], d.Serial)
+	enc.PutUint32(b[4:8], d.Refresh)
+	enc.PutUint32(b[8:12], d.Retry)
+	enc.PutUint32(b[12:16], d.Expire)
+	enc.PutUint32(b[16:20], d.Minimum)
+	buf.Write(b)
+
+	return buf.Bytes()
+
+}
